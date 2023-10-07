4c58a778c3db7cf97f758a2a019e90b6f34d70a2
Ermolaew <144021076+Ermolaew@users.noreply.github.com>
2023-10-08 00:47:20 +0300

    Update calc.js

--- Task5/calc.js
+++ Task5/calc.js
@@ -1,1 +1,37 @@
+const prices = {
+    "2jz-gte": 300000,
+    "Полумёртвая бмв": 10000,
+    "40-летняя советская гитара без первой струны": 150,
+    "Солнце": 800000000,
+    "Сухарики 3 корочки": 13
+}
 
+function setCalc(value) {
+    let domAns = document.getElementById("result")
+    domAns.replaceChildren(`${value}`);
+};
+
+function getCalc(count, currentEl) {
+    if(inputCheck(count))
+        return "Стоимость: " + String(prices[currentEl] * Number(count));
+    return "Недопустимое значение!";
+};
+
+function elements() {
+    let count = document.getElementById("count").value;
+    let currentEl = document.getElementById("choice").value;
+    let val = getCalc(count, currentEl);
+    setCalc(val);
+};
+
+function inputCheck(obj) {
+    const regex = /^[0-9]+$/;
+
+    return regex.test(obj);
+};
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM loaded");
+    let btn = document.getElementById("calculate");
+    btn.addEventListener("click", elements);
+});
