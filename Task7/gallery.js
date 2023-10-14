928e34544bd5370606242f0ed4696e142ba4bdb5
Ermolaew <144021076+Ermolaew@users.noreply.github.com>
2023-10-14 20:43:41 +0300

    Update gallery.js

--- Task7/gallery.js
+++ Task7/gallery.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function(){
+    $('.gallery').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true,
+                }
+            }
+        ]
+    });
+});
